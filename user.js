792fc11f5bb5c7328054b775457f514f21c8bb3a
Darren Ling <darrenl6131@gmail.com>
2025-10-23 17:21:17 -0400

    User profile implemented

---
+++ user.js
@@ -0,0 +1,30 @@
+// profile div
+const profileDiv = document.getElementById("profileDiv")
+// logout button
+
+const user = JSON.parse(localStorage.getItem("loggedInUser"));
+
+if (!user) {
+  // profile div not be displayed
+  profileDiv.innerHTML = "<p> No user logged in. </p>";
+} else {
+  // get user information from user object
+
+  const email = user.email;
+  const s = user.student_profile;
+  const p = user.seller_profile;
+console.log(email, s, p)
+  // edit profile div here
+ profileDiv.innerHTML = `
+   <h2> Email: ${email} </h2>
+   <h2> Name: ${s.first_name} ${s.last_name} </h2>
+   <h2> Major: ${s.major} </h2>
+  `
+}
+
+// logoutBtn
+
+// logout event listener on the button
+// everytime you click button you remove loggedUser from localStorage
+// redirect
+// localStorage.removeItem("loggedInUser");
